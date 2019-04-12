50fe0a95448c26aa93345957f9a12c5cf4367b65
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-04-12 00:06:37 -0400

    through surge deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import './style.scss';
+
+import $ from 'jquery';
+
+let num = 0;
+
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
