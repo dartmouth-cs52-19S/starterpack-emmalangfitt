6591920bf91eec777df0ebc050f8b83dcf844f7d
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-04-12 13:53:37 -0400

    react things done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import $ from 'jquery';
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-let num = 0;
-
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
